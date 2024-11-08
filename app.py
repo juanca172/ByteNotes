aca16f6d9745aa8524d80ba4049749ec8749cabb
juanca172 <61369165+juanca172@users.noreply.github.com>
2024-11-08 16:48:01 -0500

    Correccion de archivos

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for ,request, redirect
 
 app = Flask(__name__)
 
@@ -12,7 +12,16 @@ def blogs():
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    user = request.args.get('username')     # Obtiene el valor de 'username'
+    password = request.args.get('password') # Obtiene el valor de 'password'
+    
+    # Compara el usuario y la contraseña (asegúrate de que password sea string)
+    if user == "juancho" and password == "123":
+        # Redirige a la página principal (puedes cambiar 'home' por otra ruta)
+        return redirect(url_for('page'))
+    else:
+        # Si los datos son incorrectos, muestra el formulario de login
+        return render_template('login.html', error="Credenciales incorrectas")
 
 @app.route('/planes')
 def planes():
@@ -20,7 +29,22 @@ def planes():
 
 @app.route('/registro')
 def registro():
-    return render_template('registro.html')
+    user = request.args.get('username')
+    password = request.args.get('password')
+    email = request.args.get('email')
+    print(user)
+    
+    if user != None and password != None and email != None:
+        # Redirige a la página principal (puedes cambiar 'home' por otra ruta)
+        return redirect(url_for('page'))
+    else:
+        # Si los datos son incorrectos, muestra el formulario de login
+        return render_template('registro.html', error="Credenciales incorrectas")
+
+    
+@app.route('/page')
+def page():
+    return render_template('page.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
